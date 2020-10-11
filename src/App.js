c1fb7e03f10a19efe0d493c3b347e58987d9d4b1
Manoj <manojd@rgmobiles.com>
2020-10-11 21:47:56 +0530

    array,object=>spread,rest

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React from 'react'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+      marvel: ['hulk','thor'],
+      Updatemarvel:[],
+      Updatereverse:[],
+      restOpretor:[],
+      ObjectSpread :{
+        name:"mano",
+        age:55
+      },
+     
+    };
+  }
+spreadOperator=()=>{
+  var Update=[...this.state.marvel,'ironman'];
+  var UpdateRevese=['ironman',...this.state.marvel];
+  this.setState({
+    Updatemarvel: Update,Updatereverse:UpdateRevese
+  });
+}
+restOpretor=()=>{
+  var [hulk,...rest]=this.state.marvel;
+  this.setState({
+    restOpretor:hulk
+  });
+}
+ObjectSpread=()=>{
+  var add={
+    ...this.state.ObjectSpread,work:false
+ 
+}
+let {name}=this.state.ObjectSpread
+console.log("fdhf",name)
+this.setState({
+  ObjectSpread:add
+});
+}
+  render() {
+    return (
+      <div>
+        <h1>Javascript (ES6) Rest and Spread Operator...!</h1>
+        <div style={{background:"lightgreen"}}>
+          <h3>Array Methods</h3>
+    <p>Spread Operator</p>
+    <h2>{this.state.marvel}</h2>
+    <h2><button onClick={this.spreadOperator}>spreadOperator</button>{this.state.Updatemarvel}<br></br>
+   {this.state.Updatereverse}
+    </h2>
+    <h3 ><button onClick={this.restOpretor}>restOperator</button>{this.state.restOpretor}</h3>
     </div>
-  );
+    <div style={{background:"orange"}}>
+<h3>Object Method</h3>
+    <h1><button onClick={this.ObjectSpread}>ObjectSpread</button>{this.state.ObjectSpread.work}{console.log(this.state.ObjectSpread)}</h1>
+    </div>
+      </div>
+    );
+  }
 }
 
 export default App;
